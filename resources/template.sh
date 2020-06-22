35b59a05755d2ab15f90a5e26cb18671647390b6
tristan straub <tristan@straub.id.au>
2020-06-23 09:59:53 +1000

    Allow pip to be overriden

--- resources/template.sh
+++ resources/template.sh
@@ -4,6 +4,6 @@ set -euxo pipefail
 TEMPDIR=$(mktemp -d)
 clj -m clj-python-trampoline.resources --script > $TEMPDIR/trampoline.py
 clj -m clj-python-trampoline.resources --requirements > $TEMPDIR/requirements.txt
-pip3 install -r $TEMPDIR/requirements.txt --user
+${PYTHON_PIP_COMMAND:-pip3} install -r $TEMPDIR/requirements.txt --user
 
 ${PYTHON_COMMAND:-python3} $TEMPDIR/trampoline.py

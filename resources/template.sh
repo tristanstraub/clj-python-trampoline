c3c7e151c5e187b09095d9f738ea4a9925675ef7
tristan straub <tristan@straub.id.au>
2020-06-23 09:41:31 +1000

    Install javabridge with "pip3 install -r .. --user"

--- resources/template.sh
+++ resources/template.sh
@@ -4,6 +4,6 @@ set -euxo pipefail
 TEMPDIR=$(mktemp -d)
 clj -m clj-python-trampoline.resources --script > $TEMPDIR/trampoline.py
 clj -m clj-python-trampoline.resources --requirements > $TEMPDIR/requirements.txt
-pip install -r $TEMPDIR/requirements.txt
+pip3 install -r $TEMPDIR/requirements.txt --user
 
 ${PYTHON_COMMAND:-python} $TEMPDIR/trampoline.py

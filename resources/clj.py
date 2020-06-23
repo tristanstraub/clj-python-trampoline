d689742dcbdbfcacd12122d92c124044e60819e9
tristan straub <tristan@straub.id.au>
2020-06-23 14:37:57 +1000

    CLASSPATH is optional; repl is default

--- resources/clj.py
+++ resources/clj.py
@@ -1,8 +1,11 @@
-import sys, os, javabridge
+import sys, os, javabridge, subprocess
 
 def main(*args):
     jars = javabridge.JARS
     classpath=os.getenv("CLASSPATH", None)
+    if not classpath:
+        classpath = subprocess.check_output("clj -Spath", shell=True).decode('utf-8')
+
     if classpath:
         jars = jars + [classpath]
 
@@ -11,15 +14,15 @@ def main(*args):
     env = javabridge.get_env()
     stringclass = env.find_class("java/lang/String")
 
-    print('args:', args)
+    if len(args) == 0:
+        args = ["-r"]
 
-    cljargs = env.make_object_array(len(args) + 2, stringclass)
+    args = ["-e", "(require 'clj-python-trampoline.interpreter)"] + list(args)
 
-    env.set_object_array_element(cljargs, 0, env.new_string_utf("-e"))
-    env.set_object_array_element(cljargs, 1, env.new_string_utf("(require 'clj-python-trampoline.interpreter)"))
+    cljargs = env.make_object_array(len(args), stringclass)
 
     for i, arg in enumerate(args):
-        env.set_object_array_element(cljargs, i + 2, env.new_string_utf(arg))
+        env.set_object_array_element(cljargs, i, env.new_string_utf(arg))
 
     c = env.find_class("clojure/main")
     method_id = env.get_static_method_id(c, "main", "([Ljava/lang/String;)V")
